9b53a87bfafdc72070ea2f16cf1f4570ea6dcecf
Jason Drake <drake.jason@courrier.uqam.ca>
2014-10-20 17:51:54 -0400

    Création de la classe DeclarationArchitecte qui hérite de DeclarationDeFormation.

---
+++ src/declaration/DeclarationArchitecte.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package declaration;
+
+/**
+ *
+ * @author User
+ */
+public class DeclarationArchitecte extends DeclarationDeFormation {
+    
+    public DeclarationArchitecte(String numeroDepermis, String cycle, int heuresTransferees) {
+        super(numeroDepermis, cycle, heuresTransferees);
+    }
+}
