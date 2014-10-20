765dbe1a4ff044bd34ad85df8ba321d15bed6744
Jason Drake <drake.jason@courrier.uqam.ca>
2014-10-20 17:22:41 -0400

    Création de la classe ReglesPourArchitectes qui hérite de ValidateurDeFormation.

---
+++ src/validation/ReglesPourArchitectes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validation;
+
+import inf2015_projet.DeclarationDeFormation;
+
+/**
+ *
+ * @author User
+ */
+public class ReglesPourArchitectes extends ValidateurDeDeclaration {
+    
+    public ReglesPourArchitectes(DeclarationDeFormation membre) {
+        super(membre);
+    }
+}
