8805f9f77b074df83b9b8a6fc0c80d715ad6734d
Jason Drake <drake.jason@courrier.uqam.ca>
2014-10-20 17:25:51 -0400

    Création de la classe ReglesPourPsychologues qui hérite de ValidateurDeFormation.

---
+++ src/validation/ReglesPourPsychologues.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validation;
+
+import inf2015_projet.DeclarationDeFormation;
+
+/**
+ *
+ * @author User
+ */
+public class ReglesPourPsychologues extends ValidateurDeDeclaration {
+    
+    public ReglesPourPsychologues(DeclarationDeFormation membre) {
+        super(membre);
+    }
+}
