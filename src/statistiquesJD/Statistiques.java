15eadc5a3a76a4ad5ace85eaef09c27ad8f0b278
Jason Drake <drake.jason@courrier.uqam.ca>
2014-11-02 19:45:51 -0500

    Rendre fonctionelle StatistiquesJD dans le cas où le fichier de statistique est déjà existant (sans erreur).

--- src/statistiquesJD/Statistiques.java
+++ src/statistiquesJD/Statistiques.java
@@ -5,8 +5,13 @@
  */
 package statistiquesJD;
 
+import inf2015_projet.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.TreeMap;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  *
@@ -14,13 +19,13 @@ import java.util.TreeMap;
  */
 public class Statistiques {
     boolean declarationTraitee;
-    boolean declarationInvalideOuIncomplete;
+    boolean declarationIncompleteOuInvalide;
     int sexeDeclaree;
     TreeMap<String, Integer> activitesValidesParCategorie;
     
     public Statistiques() {
         declarationTraitee = false;
-        declarationInvalideOuIncomplete = false;
+        declarationIncompleteOuInvalide = false;
         activitesValidesParCategorie = new TreeMap<>();
         etablirCategoriesReconnues(activitesValidesParCategorie);
     }
@@ -44,22 +49,150 @@ public class Statistiques {
        return categories;
     }
     
-    private void enregistrerDeclarationTraitee() {
+    public void enregistrerDeclarationTraitee() {
         declarationTraitee = true;
     }
     
-    private void enregistrerDeclarationInvalideOuIncomplete() {
-        declarationInvalideOuIncomplete = true;
+    public void enregistrerDeclarationInvalideOuIncomplete() {
+        declarationIncompleteOuInvalide = true;
     }
     
-    private void enregistrerSexeDeclaree(int codeSexe) {
+    public void enregistrerSexeDeclaree(int codeSexe) {
         sexeDeclaree = codeSexe;  
     }
     
-    private void enregistrerActiviteValideParCategorie(String categorie) {
+    public void enregistrerActiviteValideParCategorie(String categorie) {
         Integer activitesValideSelonCategorie = activitesValidesParCategorie.get(categorie);
         activitesValideSelonCategorie++;
     }
     
-  
+    public void mettreAJourStatistiquesCumulatives() {
+        JSONObject donneesStatistiques = chargerStatistiquesDisponibles();
+        ajouterStatistiquesDeLaDeclarationCourante(donneesStatistiques);
+        ecrireNouvellesDonneesStatistiques(donneesStatistiques);
+    }
+
+    private static JSONObject chargerStatistiquesDisponibles() {
+        JSONObject donneesStatistiques;
+        try {
+            donneesStatistiques = chargerStatistiquesAnterieures();
+        } catch (IOException e) {
+            donneesStatistiques = construireFichierStatistiques();
+        }
+        return donneesStatistiques;
+    }
+    
+    private static JSONObject chargerStatistiquesAnterieures() throws IOException {
+        String fichierDesStatistiques = "cumulStatistiquesJD/donneesStatistiques.json";
+        String statistiquesAnterieures = FileReader.loadFileIntoString(fichierDesStatistiques, "UTF-8");
+        return JSONObject.fromObject(statistiquesAnterieures);
+    }
+
+    private static JSONObject construireFichierStatistiques() {
+        return null; //TODO: Implémenter fonction
+    }
+
+    private void ajouterStatistiquesDeLaDeclarationCourante(JSONObject donneesStatistiques) {
+        mettreAJourDeclarationsTraitees(donneesStatistiques);
+        mettreAJourDeclarationsCompletes(donneesStatistiques);
+        mettreAJourDeclarationsIncompletesOuInvalides(donneesStatistiques);
+        mettreAJourDeclarationsSelonSexe(donneesStatistiques);
+        mettreAJourActivitesValidesDesDeclarations(donneesStatistiques);
+        mettreAJourActivitesValidesParCategorie(donneesStatistiques);
+    }
+
+    private void mettreAJourDeclarationsTraitees(JSONObject donneesStatistiques) {
+        if (declarationTraitee) {
+            String champsStatistique = "declarations_traitees";
+            int declarationsTraitees = donneesStatistiques.getInt(champsStatistique);
+            donneesStatistiques.put(champsStatistique, ++declarationsTraitees);
+        }
+    }
+
+    private void mettreAJourDeclarationsCompletes(JSONObject donneesStatistiques) {
+        if (!declarationIncompleteOuInvalide) {
+            String champsStatistique = "declarations_completes";
+            int nombreAnterieur = donneesStatistiques.getInt(champsStatistique);
+            donneesStatistiques.put(champsStatistique, ++nombreAnterieur);
+        }
+    }
+
+    private void mettreAJourDeclarationsIncompletesOuInvalides(JSONObject donneesStatistiques) {
+        if (declarationIncompleteOuInvalide) {
+            String champsStatistique = "declarations_incompletes_ou_invalides";
+            int nombreAnterieur = donneesStatistiques.getInt(champsStatistique);
+            donneesStatistiques.put(champsStatistique, ++nombreAnterieur);
+        }
+    }
+
+    private void mettreAJourDeclarationsSelonSexe(JSONObject donneesStatistiques) {
+        String champsStatistique;
+        if (sexeDeclaree == 1) {
+            champsStatistique = "declarations_faites_par_des_hommes";
+        } else if (sexeDeclaree == 2) {
+            champsStatistique = "declarations_faites_par_des_femmes"; 
+        } else {
+            champsStatistique = "declarations_faites_par_des_gens_de_sexe_inconnu";
+        }
+        int nombreAnterieur = donneesStatistiques.getInt(champsStatistique);
+        donneesStatistiques.put(champsStatistique, ++nombreAnterieur);
+    }
+
+    private void mettreAJourActivitesValidesDesDeclarations(JSONObject donneesStatistiques) {
+        String champsStatistique = "activites_valides_dans_les_declarations";
+        int nombreAnterieur = donneesStatistiques.getInt(champsStatistique);
+        int activitesValidesPourDeclarationCourante = calculerNombreTotalActivitesValides();
+        donneesStatistiques.put(champsStatistique, nombreAnterieur + activitesValidesPourDeclarationCourante);
+    }
+
+    private void mettreAJourActivitesValidesParCategorie(JSONObject donneesStatistiques) {
+        String champsStatistique = "activites_valides_par_categorie";
+        JSONArray compteursActivitesValidesParCategorie = donneesStatistiques.getJSONArray(champsStatistique);
+        ArrayList<String> nomDesCategories = nomsDesCategoriesReconnues();
+        for (String categorie: nomDesCategories) {
+                mettreAJourActivitesValidesParCategorie(compteursActivitesValidesParCategorie, categorie);
+        }
+    }
+    
+    private void mettreAJourActivitesValidesParCategorie(JSONArray compteursParCategorie, String categorie) {
+        JSONObject compteurPourCategorie = trouverCompteurPourCategorie(compteursParCategorie, categorie);
+        String champsStatistique = "nombre";
+        int nombreAnterieur = compteurPourCategorie.getInt(champsStatistique);
+        int activitesValidesCourantesPourCategorie = activitesValidesParCategorie.get(categorie);
+        compteurPourCategorie.put(champsStatistique, nombreAnterieur + activitesValidesCourantesPourCategorie);
+    }
+
+    private static JSONObject trouverCompteurPourCategorie(JSONArray compteursParCategorie, String categorie) {
+        JSONObject compteurPourCategorie = null;
+        for (int i = 0; i < compteursParCategorie.size(); i++) {
+            JSONObject compteur = compteursParCategorie.getJSONObject(i);
+            String categorieCompteur = compteur.getString("categorie");
+            if (categorie.equals(categorieCompteur)) {
+                compteurPourCategorie = compteur;
+                break;
+            }
+        }
+        return compteurPourCategorie;
+    }
+    
+    private int calculerNombreTotalActivitesValides() {
+        int nombreTotalActivitesValides = 0;
+        ArrayList<String> nomDesCategories = nomsDesCategoriesReconnues();
+        for (String categorie: nomDesCategories) {
+                nombreTotalActivitesValides += activitesValidesParCategorie.get(categorie);
+        }
+        return nombreTotalActivitesValides;
+    }
+
+    private void ecrireNouvellesDonneesStatistiques(JSONObject donneesStatistiques) {
+        try {
+        FileWriter fichierDesStatistiques = new FileWriter("cumulStatistiquesJD/donneesStatistiques.json");
+        fichierDesStatistiques.write(donneesStatistiques.toString(2));
+        fichierDesStatistiques.close();
+        } catch (IOException e) {
+            // N'est pas atteingnable?
+        }
+    }
+
+    
 }
