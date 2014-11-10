dded985133aa1851eaa90a8ec5cd8653c1c38f3f
Chelny <lamajeste@gmail.com>
2014-11-10 14:26:53 -0500

    Podiatre extends Geologue

--- src/professionnels/Podiatre.java
+++ src/professionnels/Podiatre.java
@@ -1,74 +1,14 @@
 package professionnels;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import net.sf.json.JSONObject;
 
 /**
  *
  * @author Chelny Duplan, Jason Drake, Jean Mary Borgella
  */
-public class Podiatre extends Membre {
+public class Podiatre extends Geologue {
 
     public Podiatre(JSONObject activiteJson) {
         super(activiteJson);
-    }
-
-    public void ajouterActivitePourMembre(JSONObject activite) {
-        String categorie = activite.getString("categorie");
-        int temporaire = regroupementDesCategories(categorie);
-        int heures = activite.getInt("heures");
-        if (dateValidePourMembre(activite.getString("date")) && temporaire != -1 && heures > 0) {
-            activitesAcceptees.add(activite);
-        } else {
-            activitesRefusees.add(activite);
-        }
-    }
-
-    public int regroupementDesCategories(String categorie) {
-        int temporaire = -1;
-        if (premiereCategorie(categorie) == 1) {
-            temporaire = 1;
-        } else if (deuxiemeCategorie(categorie) == 2) {
-            temporaire = 2;
-        }
-        return temporaire;
-    }
-
-    private int premiereCategorie(String categorie) {
-        int temporaire = 0;
-        switch (categorie) {
-            case "atelier": case "séminaire": case "colloque": 
-            case "conférence": case "lecture dirigée":
-            case "présentation": case "rédaction professionnelle":
-                temporaire = 1;
-                break;
-        }
-        return temporaire;
-    }
-
-    private int deuxiemeCategorie(String categorie) {
-        int temporaire = 0;
-        if (categorie.equals("cours") || categorie.equals("projet de recherche")
-                || categorie.equals("groupe de discussion")) {
-            temporaire = 2;
-        }
-        return temporaire;
-    }
-    
-    private boolean dateValidePourMembre(String date) {
-        SimpleDateFormat formatISO8601 = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateLue = null;
-        Date min = null;
-        Date max = null;
-        try {
-            dateLue = formatISO8601.parse(date);
-            min = formatISO8601.parse("2013-06-01");
-            max = formatISO8601.parse("2016-06-01");
-        } catch (ParseException ex) {
-            ex.getMessage();
-        }
-        return ((dateLue.compareTo(min) >= 0) && (dateLue.compareTo(max) <= 0));
     }
 }
